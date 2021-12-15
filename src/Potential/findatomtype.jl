1742434d302a7531d8feff09e74437a4395b2f71
Valentin Churavy <v.churavy@gmail.com>
2021-12-15 11:49:02 -0500

    Revert "Update interatomic potentials"
    This reverts commit 6d2742e83e5bf5801bbb18a449b43a399f0fdc1f.

--- src/Potential/findatomtype.jl
+++
@@ -1,8 +0,0 @@
-function findatomtype(alist, atomtype, typei)
-
-atype = atomtype[alist]; # atom types of alist
-ilist = alist[atype .== typei];    # return atoms of type i
-
-return ilist
-
-end
--- src/setpath.jl
+++
@@ -1,5 +0,0 @@
-push!(LOAD_PATH, sourcepath * "src/Preprocessing");
-push!(LOAD_PATH, sourcepath * "src/Potential");
-
-
-
