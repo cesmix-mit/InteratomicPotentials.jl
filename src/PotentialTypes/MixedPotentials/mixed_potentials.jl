adbe36307eba7d24f0e2fa1bd612d68ca3f81740
Dallas Foster <fostdall@mit.edu>
2022-01-25 15:32:45 -0500

    Update MixedPotentials implementation
    Create ElementalPotential type to differentiate between single and combined potentials, both of which are subtypes of ArbitraryPotential. @jrdegreeff Note that there was something off which -LinearCombinationPotential(p2) ins the old line #10, which drove me to change mixed_potentials.jl

--- src/PotentialTypes/MixedPotentials/mixed_potentials.jl
+++ src/PotentialTypes/MixedPotentials/mixed_potentials.jl
@@ -1,18 +1,23 @@
 struct LinearCombinationPotential <: MixedPotential
-    potentials::Vector{ArbitraryPotential}
+    potentials::Vector{ElementalPotential}
     coefficients::Vector{AbstractFloat}
 end
-LinearCombinationPotential(p::ArbitraryPotential) = LinearCombinationPotential([p], [1])
+LinearCombinationPotential(p::ElementalPotential) = LinearCombinationPotential([p], [1])
 LinearCombinationPotential(p::LinearCombinationPotential) = p
 
-Base.:+(p1::ArbitraryPotential, p2::ArbitraryPotential) = LinearCombinationPotential(p1) + LinearCombinationPotential(p2)
-Base.:+(p::ArbitraryPotential) = +1 * p
-Base.:-(p1::ArbitraryPotential, p2::ArbitraryPotential) = LinearCombinationPotential(p1) + -LinearCombinationPotential(p2)
-Base.:-(p::ArbitraryPotential) = -1 * p
-Base.:*(a::Real, p::ArbitraryPotential) = a * LinearCombinationPotential(p)
-Base.:/(p::ArbitraryPotential, a::Real) = LinearCombinationPotential(p) / a
+Base.:+(p::ElementalPotential) = +1 * p
+Base.:+(mp::LinearCombinationPotential) = mp
+Base.:+(p1::ElementalPotential, p2::ElementalPotential) = LinearCombinationPotential(p1) + LinearCombinationPotential(p2)
+
+Base.:-(p::ElementalPotential) = -1 * p
+Base.:-(mp::LinearCombinationPotential) = LinearCombinationPotential(mp.potentials, -1.0*mp.coefficients)
+Base.:-(p1::ElementalPotential, p2::ElementalPotential) = LinearCombinationPotential(p1) + -LinearCombinationPotential(p2)
+
+Base.:*(a::Real, p::ElementalPotential) = a * LinearCombinationPotential(p)
+Base.:/(p::ElementalPotential, a::Real) = LinearCombinationPotential(p) / a
 
 Base.:+(mp1::LinearCombinationPotential, mp2::LinearCombinationPotential) = LinearCombinationPotential(append!(mp1.potentials, mp2.potentials), append!(mp1.coefficients, mp2.coefficients))
+Base.:-(mp1::LinearCombinationPotential, mp2::LinearCombinationPotential) = mp1 + -mp2
 Base.:*(a::Real, mp::LinearCombinationPotential) = LinearCombinationPotential(mp.potentials, a * mp.coefficients)
 Base.:/(mp::LinearCombinationPotential, a::Real) = LinearCombinationPotential(mp.potentials, mp.coefficients / a)
 
