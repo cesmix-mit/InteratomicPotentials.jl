4f987f09c326ee0c36b68d0f6559957cb89da9f7
Jeremiah DeGreeff <degreeff@mit.edu>
2022-01-25 15:53:27 -0500

    make mixed_potentials even more succinct

--- src/PotentialTypes/MixedPotentials/mixed_potentials.jl
+++ src/PotentialTypes/MixedPotentials/mixed_potentials.jl
@@ -6,33 +6,18 @@ LinearCombinationPotential(p::ArbitraryPotential) = LinearCombinationPotential([
 LinearCombinationPotential(p::LinearCombinationPotential) = p
 
 Base.:+(p::ArbitraryPotential) = +1 * p
-Base.:+(mp::LinearCombinationPotential) = mp
-Base.:+(p1::ArbitraryPotential, p2::ArbitraryPotential) = LinearCombinationPotential(p1) + LinearCombinationPotential(p2)
-
 Base.:-(p::ArbitraryPotential) = -1 * p
-Base.:-(mp::LinearCombinationPotential) = LinearCombinationPotential(mp.potentials, -1.0*mp.coefficients)
-Base.:-(p1::ArbitraryPotential, p2::ArbitraryPotential) = LinearCombinationPotential(p1) + -LinearCombinationPotential(p2)
-
+Base.:+(p1::ArbitraryPotential, p2::ArbitraryPotential) = LinearCombinationPotential(p1) + LinearCombinationPotential(p2)
+Base.:-(p1::ArbitraryPotential, p2::ArbitraryPotential) = p1 + -p2
 Base.:*(a::Real, p::ArbitraryPotential) = a * LinearCombinationPotential(p)
-Base.:/(p::ArbitraryPotential, a::Real) = LinearCombinationPotential(p) / a
+Base.:/(p::ArbitraryPotential, a::Real) = (1.0 / a) * p
 
 Base.:+(mp1::LinearCombinationPotential, mp2::LinearCombinationPotential) = LinearCombinationPotential(append!(mp1.potentials, mp2.potentials), append!(mp1.coefficients, mp2.coefficients))
-Base.:-(mp1::LinearCombinationPotential, mp2::LinearCombinationPotential) = mp1 + -mp2
 Base.:*(a::Real, mp::LinearCombinationPotential) = LinearCombinationPotential(mp.potentials, a * mp.coefficients)
-Base.:/(mp::LinearCombinationPotential, a::Real) = LinearCombinationPotential(mp.potentials, mp.coefficients / a)
 
-function potential_energy(sys::AbstractSystem, mp::LinearCombinationPotential)
-    sum(c * potential_energy(sys, p) for (p, c) in zip(mp.potentials, mp.coefficients))
-end
+apply_linear_combination(f::Function, sys::AbstractSystem, mp::LinearCombinationPotential) = sum(c * f(sys, p) for (p, c) in zip(mp.potentials, mp.coefficients))
 
-function force(sys::AbstractSystem, mp::LinearCombinationPotential)
-    sum(c .* force(sys, p) for (p, c) in zip(mp.potentials, mp.coefficients))
-end
-
-function virial(sys::AbstractSystem, mp::LinearCombinationPotential)
-    sum(c * virial(sys, p) for (p, c) in zip(mp.potentials, mp.coefficients))
-end
-
-function virial_stress(sys::AbstractSystem, mp::LinearCombinationPotential)
-    sum(c .* virial_stress(sys, p) for (p, c) in zip(mp.potentials, mp.coefficients))
-end
+potential_energy(sys::AbstractSystem, mp::LinearCombinationPotential) = apply_linear_combination(potential_energy, sys, mp)
+force(sys::AbstractSystem, mp::LinearCombinationPotential) = apply_linear_combination(force, sys, mp)
+virial(sys::AbstractSystem, mp::LinearCombinationPotential) = apply_linear_combination(virial, sys, mp)
+virial_stress(sys::AbstractSystem, mp::LinearCombinationPotential) = apply_linear_combination(virial_stress, sys, mp)
