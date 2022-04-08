9f64671cfe7e82dc63437e0a51e771f88208a0e3
Jeremiah DeGreeff <degreeff@mit.edu>
2022-04-08 13:03:08 -0400

    simplify directory structure

--- src/InteratomicPotentials.jl
+++ src/InteratomicPotentials.jl
@@ -12,13 +12,13 @@ using NearestNeighbors
 # Import files
 include("unit_convention.jl")
 include("constants.jl")
-include("NeighborList/nnlist.jl")
-include("Parameters/param.jl")
-include("PotentialTypes/types.jl")
+include("nnlist.jl")
+include("types.jl")
 
 # Export energies, forces, virial evaluations 
 export energy_and_force, potential_energy, force, virial, virial_stress
 
+# Export parameter manipulation functions
 export get_parameters, set_parameters, serialize_parameters, deserialize_parameters
 export get_hyperparameters, set_hyperparameters, serialize_hyperparameters, deserialize_hyperparameters
 
