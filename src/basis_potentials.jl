fc2ec2b3538c683e221b65957d65f1167309aabc
Jeremiah DeGreeff <degreeff@mit.edu>
2022-04-28 12:01:32 -0400

    InteratomicPotentials v0.2 interface

--- src/basis_potentials.jl
+++ src/basis_potentials.jl
@@ -4,15 +4,15 @@ abstract type BasisParameters end
 ################################################################################
 # InteratomicPotentials API implmentations for basis potentials
 ################################################################################
-import InteratomicPotentials.energy_and_force
-import InteratomicPotentials.virial_stress
-function InteratomicPotentials.energy_and_force(A::AbstractSystem, p::BasisPotential)
+import InteratomicPotentials: energy_and_force, virial_stress, ENERGY_UNIT, FORCE_UNIT
+
+function energy_and_force(A::AbstractSystem, p::BasisPotential)
     B, dB, _ = evaluate_full(A, p.basis_params)
-    e = sum(B) ⋅ p.coefficients
-    f = [SVector{3}(di' * p.coefficients) for di in dB]
+    e = sum(B) ⋅ p.coefficients * ENERGY_UNIT
+    f = [SVector{3}(di' * p.coefficients) for di in dB] * FORCE_UNIT
     (; e, f)
 end
 
 function virial_stress(A::AbstractSystem, p::BasisPotential)
-    sum(SVector{6}(di ⋅ p.coefficients) for di in evaluate_basis_v(A, p.basis_params))
+    sum(SVector{6}(di ⋅ p.coefficients) for di in evaluate_basis_v(A, p.basis_params)) * ENERGY_UNIT
 end
