4ad83dec42fed188a8b5dbc3561c18d5e6a36f7b
Jeremiah DeGreeff <degreeff@mit.edu>
2022-05-09 11:57:00 -0400

    tweak sample sizes in benchmarks

--- benchmarks/lj_1000.jl
+++ benchmarks/lj_1000.jl
@@ -2,8 +2,10 @@ include("helpers.jl")
 
 using BenchmarkTools
 
+T, = parse.(Int, ARGS)
+
 potential = LennardJones(1.0u"eV", 1.0u"Å", 100.0u"Å", [:Ar])
 
 system = load_configuration("benchmarks/data/lj_clusters/1000.xyz")
 
-display(@benchmark energy_and_force($system, $potential))
+display(@benchmark energy_and_force($system, $potential) seconds = T)
