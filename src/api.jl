4bf7512159dc610237aa695819f32f087d5a0e8a
Dallas Foster <fostdall@mit.edu>
2022-10-06 16:43:57 -0400

    Update api.jl
    Rename get_local_descriptors, ... to compute_local descriptors.

--- src/api.jl
+++ src/api.jl
@@ -4,7 +4,7 @@ export energy_and_force, potential_energy, force, virial, virial_stress
 export get_rcutoff, get_species
 export get_parameters, set_parameters, serialize_parameters, deserialize_parameters
 export get_hyperparameters, set_hyperparameters, serialize_hyperparameters, deserialize_hyperparameters
-export get_local_descriptors, get_force_descriptors, get_virial_descriptors, get_all_descriptors
+export compute_local_descriptors, compute_force_descriptors, compute_virial_descriptors, compute_all_descriptors
 """
     energy_and_force(s::AbstractSystem, p::AbstractPotential)::NamedTuple{(:e, :f), Tuple{Unitful.Energy,Vector{SVector{3, Unitful.Force}}}}
 
@@ -143,27 +143,27 @@ Compute the local descriptors for an abstract system s using the basis system de
 
 For a system s with n_atom number of atoms this method get_local_descriptors returns a Vector{Vector{T<:Real}}, containing n_atom vectors each of dimension d as defined by the basis system.
 """
-function get_local_descriptors end 
+function compute_local_descriptors end 
 """
-    get_force_descriptors(s::AbstractSystem, b::BasisSystem)::Vector{Vector{Vector}}
+    compute_force_descriptors(s::AbstractSystem, b::BasisSystem)::Vector{Vector{Vector}}
 
 Compute the force descriptors for an abstract system s using the basis system defined in b. These types are defined in this package, but the primary use is in the InteratomicBasisPotentials.jl package, where the individual basis systems are defined. 
 
-For a system s with n_atom number of atoms this method get_force_descriptors returns a Vector{Matrix{T<:Real}}}, containing n_atom vectors each containing a matrix with rows corresponding to x-, y-, and z-components of dimension d as defined by the basis system.
+For a system s with n_atom number of atoms this method compute_force_descriptors returns a Vector{Matrix{T<:Real}}}, containing n_atom vectors each containing a matrix with rows corresponding to x-, y-, and z-components of dimension d as defined by the basis system.
 """
-function get_force_descriptors end
+function compute_force_descriptors end
 """
-    get_virial_descriptors(s::AbstractSystem, b::BasisSystem)::Vector{Vector{Vector}}
+    compute_virial_descriptors(s::AbstractSystem, b::BasisSystem)::Vector{Vector{Vector}}
 
 Compute the virial descriptors for an abstract system s using the basis system defined in b. These types are defined in this package, but the primary use is in the InteratomicBasisPotentials.jl package, where the individual basis systems are defined. 
 
-For a system s with n_atom number of atoms this method get_virial_descriptors returns a Matrix{T<:Real}, containing 6 rows corresponding to entries of the stress tensor and columns of dimension d as defined by the basis system.
+For a system s with n_atom number of atoms this method compute_virial_descriptors returns a Matrix{T<:Real}, containing 6 rows corresponding to entries of the stress tensor and columns of dimension d as defined by the basis system.
 """
-function get_virial_descriptors end
+function compute_virial_descriptors end
 """
-    get_all_descriptors(s::AbstractSystem, b::BasisSystem)::Vector{Vector{Vector}}
+    compute_all_descriptors(s::AbstractSystem, b::BasisSystem)::Vector{Vector{Vector}}
 
 Compute the local, force, and virial descriptors for an abstract system s using the basis system defined in b. These types are defined in this package, but the primary use is in the InteratomicBasisPotentials.jl package, where the individual basis systems are defined. 
 """
-function get_all_descriptors end
+function compute_all_descriptors end
 
