fddcd3b2d1d20281958a4e53e0ce8207b7a38f68
Emmanuel Lujan <lujan.emmanuel@gmail.com>
2023-12-04 22:59:52 -0500

    Update of doc. conf. file.

--- docs/make.jl
+++ docs/make.jl
@@ -5,44 +5,58 @@ using Documenter
 using DocumenterCitations
 using Literate
 
-DocMeta.setdocmeta!(InteratomicPotentials, :DocTestSetup, :(using InteratomicPotentials); recursive = true)
+DocMeta.setdocmeta!(
+    InteratomicPotentials,
+    :DocTestSetup,
+    :(using InteratomicPotentials);
+    recursive = true,
+)
 
-bib = CitationBibliography(joinpath(@__DIR__, "citations.bib"))
+bib = CitationBibliography(joinpath(@__DIR__, "citation.bib"))
 
 # Generate examples
 
-const EXAMPLES_DIR = joinpath(@__DIR__, "..", "examples")
-const OUTPUT_DIR = joinpath(@__DIR__, "src/generated")
+#const EXAMPLES_DIR = joinpath(@__DIR__, "..", "examples")
+#const OUTPUT_DIR = joinpath(@__DIR__, "src/generated")
 
-examples = Pair{String,String}[]
+##examples = Pair{String,String}[]
+#examples = [
+#    "LJCluster" => "LJCluster"
+#]
 
-for (_, name) in examples
-    example_filepath = joinpath(EXAMPLES_DIR, string(name, ".jl"))
-    Literate.markdown(example_filepath, OUTPUT_DIR, documenter = true)
-end
+#for (_, name) in examples
+#    example_filepath = joinpath(EXAMPLES_DIR, string(name, ".jl"))
+#    Literate.markdown(example_filepath, OUTPUT_DIR, documenter = true)
+#end
 
-examples = [title => joinpath("generated", string(name, ".md")) for (title, name) in examples]
+examples = [title => joinpath("generated", string(name, ".md"))
+            for (title, name) in examples]
 
-makedocs(bib;
-    modules = [InteratomicPotentials],
-    authors = "CESMIX-MIT",
-    repo = "https://github.com/cesmix-mit/InteratomicPotentials.jl/blob/{commit}{path}#{line}",
-    sitename = "InteratomicPotentials.jl",
-    format = Documenter.HTML(;
-        prettyurls = get(ENV, "CI", "false") == "true",
-        canonical = "https://cesmix-mit.github.io/InteratomicPotentials.jl",
-        assets = String[],
-    ),
-    pages = [
+makedocs(
+      root    =  joinpath(dirname(pathof(PotentialLearning)), "..", "docs"),
+      source  = "src",
+      build   = "build",
+      clean   = true,
+      doctest = true,
+      modules = [InteratomicPotentials],
+      repo    = "https://github.com/cesmix-mit/InteratomicPotentials.jl/blob/{commit}{path}#{line}",
+      highlightsig = true,
+      sitename = "InteratomicPotentials.jl",
+      expandfirst = [],
+      draft = false,
+      pages = [
         "Home" => "index.md",
         "InteratomicPotentials Interface" => "interface.md",
         "Examples" => examples,
         "API Reference" => "api.md",
         "References" => "bibliography.md"
-    ],
-    doctest = true,
-    linkcheck = true,
-    strict = true
+      ],
+      format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
+        canonical = "https://cesmix-mit.github.io/InteratomicPotentials.jl",
+        assets = String[],
+      ),
+      plugins=[bib]
 )
 
 deploydocs(;
