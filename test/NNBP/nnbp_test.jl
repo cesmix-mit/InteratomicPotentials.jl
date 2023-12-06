6b37df8f8739418ad4af40334863f36a3b26fc2a
Emmanuel Lujan <lujan.emmanuel@gmail.com>
2023-12-06 18:43:30 -0500

    improving neural network basis potential test

--- test/NNBP/nnbp_test.jl
+++ test/NNBP/nnbp_test.jl
@@ -5,8 +5,8 @@ using UnitfulAtomic
 using InteratomicPotentials
 using Flux
 
+# Define atomic system #########################################################
 
-# Define atomic system
 r0 = 2.0
 position0 = @SVector [1.0, 0.0, 0.0]
 element = :Ar
@@ -21,10 +21,44 @@ box = [[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]]
 bcs = [Periodic(), Periodic(), Periodic()]
 system = FlexibleSystem(atoms, box * u"Å", bcs)
 
+# Compose Neural ACE ###########################################################
+
 # Define basis system
 basis = ACE( species = [:Ar], body_order = 3, polynomial_degree = 3, 
              wL = 1.0, csp = 1.0, r0 = 0.4, rcutoff = 2.0)
 n_desc = 5
+
+# Define neural network besis potential
+nns = Dict()
+species = [element]
+for s in species
+    nns[s] = Chain( Dense(n_desc,128,σ; init = Flux.glorot_uniform(gain=-10)),
+                    Dense(128,128,σ; init = Flux.glorot_uniform(gain=-10)),
+                    Dense(128,1; init = Flux.glorot_uniform(gain=-10), bias = false))
+end
+nnbp = NNBasisPotential(nns, basis)
+@test isa(nnbp, NNBasisPotential)
+
+# Compute potential energy
+@test isa(potential_energy(system, nnbp), Number)
+
+# Compose Neural SNAP ##########################################################
+
+# Define basis system
+num_elements = 1
+num_atoms = 3
+twojmax = 4
+rcutfac = 1.2
+radii = [1.5]
+rcut0 = 0.989
+weight = [1.0]
+chem_flag = false
+bzero_flag = false
+bnorm_flag = false
+basis = SNAP(num_atoms, twojmax, [:Ar], rcutfac, 0.00, rcut0, radii, weight,
+             chem_flag, bzero_flag, bnorm_flag)
+
+n_desc = length(snap)
 
 # Define neural network besis potential
 nns = Dict()
