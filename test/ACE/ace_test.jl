01f4ac947a4793ca593e1f2c9423209749e4e93a
Spencer Wyant <17836774+swyant@users.noreply.github.com>
2024-01-25 18:03:31 -0500

    updated some existing ACE tests

--- test/ACE/ace_test.jl
+++ test/ACE/ace_test.jl
@@ -23,14 +23,23 @@ ace = ACE( species = [:Ar],
            polynomial_degree = 8,
            rcutoff = 2.0)
 @test isa(ace, BasisSystem)
-e = sum(compute_local_descriptors(system, ace))
-@test isa(e, AbstractVector)
-f = compute_local_descriptors(system, ace)
-@test all(isa.(f, (AbstractVector,)))
+e_descrs_l = compute_local_descriptors(system, ace)
+@test all(isa.(e_descrs_l, Vector{<:Real}))
+@test length(e_descrs_l) == 3
+@test length(e_descrs_l[1]) == 8
+e_descrs_g = sum(e_descrs_l)
+@test isa(e_descrs_g, AbstractVector)
+
+f_descrs = compute_force_descriptors(system, ace)
+@test typeof(f_descrs) <: Vector{<:Vector{<:Vector{<:Real}}}
+@test length(f_descrs) == 3 # num of atoms
+@test length(f_descrs[1]) == 3 # x,y,z
+@test length(f_descrs[1][1]) == 8 # number of descriptors
+
 v = compute_virial_descriptors(system, ace)
 @test isa(v, AbstractArray)
 
-print(ace)
+#print(ace)
 lbp = LBasisPotential(ace)
 @test isa(lbp, BasisPotential)
 
