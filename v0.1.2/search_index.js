e32909f3fa36f30ff44233e3221f44c43031b841
Documenter.jl <documenter@juliadocs.github.io>
2025-05-05 14:19:26 +0000

    delete history

---
+++ v0.1.2/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"api/#API","page":"API","title":"API","text":"","category":"section"},{"location":"api/","page":"API","title":"API","text":"Modules = [InteratomicPotentials]","category":"page"},{"location":"#InteratomicPotentials","page":"Home","title":"InteratomicPotentials","text":"","category":"section"}]
+}
